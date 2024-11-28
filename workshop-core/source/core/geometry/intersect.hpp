1ee9a26199b1c01030887912607af967a8016bee
Daniil-SV <daniilsivacenko@gmail.com>
2024-11-28 17:58:17 +0200

    added some basic functions for 2d geometry calculations

---
+++ workshop-core/source/core/geometry/intersect.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "math.hpp"
+
+#include <optional>
+#include <tuple>
+
+namespace wk
+{
+	// https://flassari.is/2008/11/line-line-intersection-in-cplusplus
+	std::optional<PointF> line_intersect(const LineF l1, const LineF l2)
+	{
+		float x1 = l1.start.x, x2 = l1.end.x, x3 = l2.start.x, x4 = l2.end.x;
+		float y1 = l1.start.y, y2 = l1.end.y, y3 = l2.start.y, y4 = l2.end.y;
+
+		float determinant = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+
+		if (determinant == 0)
+		{
+			return std::nullopt;
+		};
+
+		// Get the x and y
+		float pre = (x1 * y2 - y1 * x2), post = (x3 * y4 - y3 * x4);
+		float x = (pre * (x3 - x4) - (x1 - x2) * post) / determinant;
+		float y = (pre * (y3 - y4) - (y1 - y2) * post) / determinant;
+
+		// Check if the x and y coordinates are within both lines
+		if (x < std::min(x1, x2) || x > std::max(x1, x2) ||
+			x < std::min(x3, x4) || x > std::max(x3, x4))
+		{
+			return std::nullopt;
+		};
+
+		if (y < std::min(y1, y2) || y > std::max(y1, y2) ||
+			y < std::min(y3, y4) || y > std::max(y3, y4))
+		{
+			return std::nullopt;
+		};
+
+		return PointF(x, y);
+	}
+
+	std::optional<std::tuple<size_t, size_t, PointF>> line_intersect(const std::vector<Point>& polygon, const LineF line)
+	{
+		size_t length = polygon.size();
+		for (size_t i = 0; length > i; i++)
+		{
+			size_t p1_idx = i;
+			size_t p2_idx = (i + 1) % length;
+
+			const Point& p1 = polygon[p1_idx];
+			const Point& p2 = polygon[p2_idx];
+
+			LineF candidate(
+				LineF::ValueT((float)p1.x, (float)p1.y),
+				LineF::ValueT((float)p2.x, (float)p2.y)
+			);
+
+			auto result = line_intersect(candidate, line);
+			if (result.has_value())
+			{
+				return std::make_tuple(p1_idx, p2_idx, result.value());
+			}
+		}
+
+		return std::nullopt;
+	}
+}
---
+++ workshop-core/source/core/geometry/math.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "core/math/point.h"
+#include "core/math/line.h"
+#include "core/math/triangle.h"
+
+#include <vector>
+#include <cmath>
+
+namespace wk::Geometry
+{
+	template<typename T1, typename T2>
+	float dist(const Point_t<T1>& p1, const Point_t<T2>& p2) {
+		return std::sqrt(std::pow(p1.x - p2.x, 2) + std::pow(p1.y - p2.y, 2));
+	}
+
+	template<typename T>
+	float line_angle(const Line_t<T>& line)
+	{
+		return std::atan2(line.end.y - line.start.y, line.end.x - line.start.x);
+	}
+
+	template<typename T>
+	Triangle_t<T> build_triangle(const Line_t<T>& bisector, float angle, const T& length)
+	{
+		T half_length = (T)((float)length / 2);
+
+		const Point_t<T>& midpoint = bisector.end;
+
+		T x1 = midpoint.x + half_length * std::cos(angle);
+		T y1 = midpoint.y + half_length * std::sin(angle);
+
+		T x2 = midpoint.x - half_length * std::cos(angle);
+		T y2 = midpoint.y - half_length * std::sin(angle);
+
+		return Triangle_t<T>(
+			bisector.start,
+			Point_t<T>(x1, y1),
+			Point_t<T>(x2, y2)
+		);
+	}
+
+	template<typename T>
+	bool point_inside_polygon(const std::vector<Point_t<T>> polygon, Point_t<T> point)
+	{
+		bool result = false;
+
+		for (size_t i = 0, j = polygon.size() - 1; i < polygon.size(); j = i++)
+		{
+			const auto& p1 = polygon[i];
+			const auto& p2 = polygon[j];
+
+			bool intersect = ((p1.y > point.y) != (p2.y > point.y)) && (point.x < (p2.x - p1.x) * (point.y - p1.y) / (p2.y - p1.y) + p1.x);
+			if (intersect)
+			{
+				result = !result;
+			}
+		}
+
+		return result;
+	}
+}
