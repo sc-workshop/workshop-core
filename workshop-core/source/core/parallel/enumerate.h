5140a9273b4e69bc9bf3af8b16978c6ee79f4bad
Daniil-SV <daniilsivacenko@gmail.com>
2024-12-07 16:06:37 +0200

    added enumerate function for parallel processing

---
+++ workshop-core/source/core/parallel/enumerate.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <functional>
+#include <vector>
+#include <future>
+
+namespace wk::parallel
+{
+	template<class T>
+	using TIteratorValue = typename std::iterator_traits<T>::value_type;
+
+	template<class Iterator>
+	inline void enumerate(
+		Iterator from, Iterator to,
+		std::function<void(TIteratorValue<Iterator>, size_t)> fn,
+		std::launch policy = std::launch::deferred | std::launch::async)
+	{
+		size_t task_count = std::distance(from, to);
+
+		std::vector<std::future<void>> tasks(task_count);
+
+		auto it = from;
+		for (size_t i = 0; i < task_count; i++) {
+			tasks[i] = std::async(policy, fn, *it++, unsigned(i));
+		}
+
+		for (size_t i = 0; i < task_count; ++i) tasks[i].wait();
+
+	}
+}
