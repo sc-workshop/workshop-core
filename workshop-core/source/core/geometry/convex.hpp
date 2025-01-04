3f9a38e7f95fb2a930142ea03e7039540bbd7a40
Daniil-SV <daniilsivacenko@gmail.com>
2025-01-04 23:30:21 +0200

    Added convex generate functions

---
+++ workshop-core/source/core/geometry/convex.hpp
@@ -0,0 +1,120 @@
+#pragma once
+
+#include "math.hpp"
+
+#include <optional>
+#include <tuple>
+
+namespace wk::Geometry
+{
+	enum PolygonType
+	{
+		Convex,
+		Concave,
+		Complex
+	};
+
+	template<typename T>
+	PolygonType get_polygon_type(std::vector<Point_t<T>> points)
+	{
+		bool sign = false;
+		for (size_t i = 0; points.size() > i; i++)
+		{
+			Point_t<T> p1 = points[i];
+			Point_t<T> p2 = points[(i + 1) % points.size()];
+			Point_t<T> p3 = points[(i + 2) % points.size()];
+			float cross = cross_product(p1, p2, p3);
+
+			if (i == 0)
+			{
+				sign = cross > 0;
+			}
+			else if (sign != cross > 0)
+			{
+				return PolygonType::Complex;
+			}
+		}
+
+		return sign ? PolygonType::Convex : PolygonType::Concave;
+	}
+
+	namespace Hull
+	{
+		template<typename T>
+		void quickHull(const std::vector<Point_t<T>>& v, const Point_t<T>& a, const Point_t<T>& b, std::vector<Point_t<T>>& hull)
+		{
+			if (v.empty()) {
+				return;
+			}
+
+			auto f = v[get_farthest_point(Line_t<T>(a, b), v)];
+
+			// Collect points to the left of segment (a, f)
+			std::vector<Point_t<T>> left;
+			for (auto p : v) {
+				if (cross_product(a, f, p) > 0) {
+					left.push_back(p);
+				}
+			}
+			quickHull(left, a, f, hull);
+
+			// Add f to the hull
+			hull.push_back(f);
+
+			// Collect points to the left of segment (f, b)
+			std::vector<Point_t<T>> right;
+			for (auto p : v) {
+				if (cross_product(f, b, p) > 0) {
+					right.push_back(p);
+				}
+			}
+			quickHull(right, f, b, hull);
+		}
+
+		template<typename T>
+		std::vector<Point_t<T>> quick_hull(std::vector<Point_t<T>> points)
+		{
+			std::vector<Point_t<T>> result;
+			if (points.size() < 3) return result;
+
+			// Find the leftmost and rightmost points
+			size_t min = 0;
+			size_t max = 0;
+			for (size_t i = 1; points.size() > i; i++)
+			{
+				if (points[i].x < points[min].x)
+				{
+					min = i;
+				}
+				if (points[i].x > points[max].x)
+				{
+					max = i;
+				}
+			}
+
+			// Split the points into two parts
+			std::vector<Point_t<T>> left;
+			std::vector<Point_t<T>> right;
+			for (size_t i = 0; points.size() > i; i++)
+			{
+				if (cross_product(points[min], points[max], points[i]) > 0)
+				{
+					left.push_back(points[i]);
+				}
+				else if (cross_product(points[min], points[max], points[i]) < 0)
+				{
+					right.push_back(points[i]);
+				}
+			}
+
+			// Find the hull
+			result.push_back(points[min]);
+			quickHull(left, points[min], points[max], result);
+
+			result.push_back(points[max]);
+			quickHull(right, points[max], points[min], result);
+
+			return result;
+		}
+	}
+}
--- workshop-core/source/core/geometry/math.hpp
+++ workshop-core/source/core/geometry/math.hpp
@@ -9,17 +9,67 @@
 
 namespace wk::Geometry
 {
+	/// <summary>
+	/// Z Cross product of two vectors.
+	/// </summary>
+	/// <typeparam name="T1"></typeparam>
+	/// <typeparam name="T2"></typeparam>
+	/// <typeparam name="T3"></typeparam>
+	/// <param name="p1"></param>
+	/// <param name="p2"></param>
+	/// <param name="p3"></param>
+	/// <returns></returns>
+	template<typename T1, typename T2, typename T3>
+	float cross_product(const Point_t<T1>& p1, const Point_t<T2>& p2, const Point_t<T3>& p3)
+	{
+		return (float)(
+			(p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x)
+		);
+	}
+
+	/// <summary>
+	/// Calculates the Euclidean distance between two points.
+	/// </summary>
+	/// <typeparam name="T1"></typeparam>
+	/// <typeparam name="T2"></typeparam>
+	/// <param name="p1"></param>
+	/// <param name="p2"></param>
+	/// <returns>Distance between two points</returns>
 	template<typename T1, typename T2>
 	float dist(const Point_t<T1>& p1, const Point_t<T2>& p2) {
 		return (float)std::sqrt(std::pow(p1.x - p2.x, 2) + std::pow(p1.y - p2.y, 2));
 	}
 
+	template<typename T1, typename T2>
+	float line_length(const Point_t<T1>& a, const Point_t<T2>& b) {
+		return std::sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y));
+	}
+
+	template<typename T1, typename T2, typename T3>
+	float point_to_line_distance(const Point_t<T1>& a, const Point_t<T2>& b, const Point_t<T3>& p) {
+		return (float)std::fabs((b.x - a.x) * (a.y - p.y) - (b.y - a.y) * (a.x - p.x)) / line_length(a, b);
+	}
+
+	/// <summary>
+	/// Calculates the angle of a line.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="line"></param>
+	/// <returns>Angle in radians</returns>
 	template<typename T>
 	float line_angle(const Line_t<T>& line)
 	{
 		return (float)std::atan2(line.end.y - line.start.y, line.end.x - line.start.x);
 	}
 
+	/// <summary>
+	/// Builds triangle by given bisector, angle and length.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="bisector"></param>
+	/// <param name="angle">Angle in radians</param>
+	/// <param name="length">Triangle length</param>
+	/// <returns></returns>
 	template<typename T>
 	Triangle_t<T> build_triangle(const Line_t<T>& bisector, float angle, const T& length)
 	{
@@ -40,6 +90,13 @@ namespace wk::Geometry
 		);
 	}
 
+	/// <summary>
+	/// Calculates if point is inside polygon.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="polygon"></param>
+	/// <param name="point"></param>
+	/// <returns>True if point is inside polygon</returns>
 	template<typename T>
 	bool point_inside_polygon(const std::vector<Point_t<T>> polygon, Point_t<T> point)
 	{
@@ -58,5 +115,21 @@ namespace wk::Geometry
 		}
 
 		return result;
+	}
+
+	template<typename T>
+	size_t get_farthest_point(const Line_t<T> line, const std::vector<Point_t<T>>& v) {
+		size_t idxMax = 0;
+		float distMax = point_to_line_distance(line.start, line.end, v[idxMax]);
+
+		for (size_t i = 1; i < v.size(); ++i) {
+			float distCurr = point_to_line_distance(line.start, line.end, v[i]);
+			if (distCurr > distMax) {
+				idxMax = i;
+				distMax = distCurr;
+			}
+		}
+
+		return idxMax;
 	}
 }
