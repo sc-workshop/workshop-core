6b1cbc00226924b905664d9a690e1a7fb7f73f64
Daniil-SV <daniilsivacenko@gmail.com>
2025-08-26 16:06:20 +0300

    extended api for thread pool

--- workshop-core/source/core/parallel/thread_pool.cpp
+++ workshop-core/source/core/parallel/thread_pool.cpp
@@ -8,40 +8,113 @@ namespace wk {
 
 	ThreadPool::~ThreadPool()
 	{
-		stop = true;
-		for (auto& thread : m_threads) {
-			thread.join();
-		}
+		if (!stop)
+			join();
 	}
 
 	void ThreadPool::setup_threads(size_t count)
 	{
 		std::lock_guard guard(mut);
 
+		stop = false;
 		m_threads.clear();
 		m_threads.reserve(count);
 
 		for (size_t i = 0; count > i; i++) {
-			m_threads.emplace_back(ThreadPool::worker_function, std::ref(*this));
+			auto& thread = m_threads.emplace_back();
+			thread.handle = std::thread(ThreadPool::worker_function, std::ref(*this), std::ref(thread));
 		}
 	}
 
-	void ThreadPool::worker_function(ThreadPool& pool)
+	void ThreadPool::worker_function(ThreadPool& pool, ThreadContext& context)
 	{
-		while (!pool.stop) {
+		while (true) {
 			std::function<void()> fn;
 			{
 				std::unique_lock guard(pool.mut);
-				pool.m_condition.wait(guard, [&pool]() {
-					return !pool.m_tasks.empty();
+				pool.m_input.wait(guard, [&pool]() {
+					return pool.should_stop() || !pool.m_tasks.empty();
 				});
 
+				if (pool.stop && pool.m_tasks.empty()) {
+					return;
+				}
+					
+
 				fn = std::move(pool.m_tasks.front());
+				context.active = true;
 				pool.m_tasks.pop();
 			}
 
-			fn();
+			try {
+				fn();
+			}
+			catch (...) {
+				pool.m_exception = std::current_exception();
+				return;
+			}
+
+			context.active = false;
+			pool.m_output.notify_all();
 		}
+	}
+
+	bool ThreadPool::should_stop()
+	{
+		return stop || m_exception;
+	}
+
+	void ThreadPool::handle_pool_break()
+	{
+		if (m_exception) {
+			std::rethrow_exception(m_exception);
+		}
+		else {
+			throw wk::Exception("Pool is stopped and cannot be used again");
+		}
+	}
+
+	size_t ThreadPool::active_threads() const
+	{
+		size_t result = 0;
+		for (const auto& thread : m_threads) {
+			if (thread.active)
+				result++;
+		}
+		return result;
+	}
+
+	void ThreadPool::join() {
+		stop = true;
+		m_input.notify_all();
+		for (auto& thread : m_threads) {
+			thread.handle.join();
+		}
+	}
+
+	void ThreadPool::wait() {
+		if (m_tasks.empty()) return;
+
+		std::unique_lock guard(mut);
+		m_output.wait(guard, [&]() {
+			if (should_stop())
+				handle_pool_break();
+
+			return m_tasks.empty() && active_threads() == 0;
+		});
+	}
+
+	size_t ThreadPool::size() const {
+		return m_threads.size();
+	}
+
+	void ThreadPool::resize(size_t count)
+	{
+		if (size() == count)
+			return;
+
+		join();
+		setup_threads(count);
 	}
 }
 
--- workshop-core/source/core/parallel/thread_pool.h
+++ workshop-core/source/core/parallel/thread_pool.h
@@ -7,36 +7,75 @@
 #include <functional>
 #include <condition_variable>
 #include <iostream>
+#include "core/memory/ref.h"
+#include "core/exception/exception.h"
 
 namespace wk {
+	struct ThreadContext {
+		bool active = false;
+		std::thread handle;
+	};
+
 	class ThreadPool {
 	public:
-		ThreadPool(size_t count);
+		static inline size_t defaultThreadCount = std::thread::hardware_concurrency();
+
+		// Global top-level pool
+		static ThreadPool& Instance() {
+			static ThreadPool instance;
+			return instance;
+		}
+
+	public:
+		ThreadPool(size_t count = ThreadPool::defaultThreadCount);
 		~ThreadPool();
 
 	public:
 		template<typename Fn, typename... Args> 
-		void push(Fn& fn, Args&... args){
+		void push(Fn&& fn, Args&&... args){
+			if (should_stop())
+				handle_pool_break();
+
 			auto callable = [=]() {
 				fn(args...);
 			};
 
 			std::lock_guard guard(mut);
 			m_tasks.push(callable);
-			m_condition.notify_one();
+			m_input.notify_one();
 		}
+
+		// Finish all the tasks and wait workers to exit
+		void join();
+
+		// Change count of worker threads
+		void resize(size_t count);
+
+		// Current coutn of worker threads
+		size_t size() const;
+
+		// Wait for all tasks to be finished
+		void wait();
+
+		// Count of working threads
+		size_t active_threads() const;
 
 	private:
 		void setup_threads(size_t count);
+		static void worker_function(ThreadPool& pool, ThreadContext& context);
 
-		static void worker_function(ThreadPool& pool);
+		bool should_stop();
+		void handle_pool_break();
 
 	private:
 		std::mutex mut;
 
-		std::vector<std::thread> m_threads;
+		std::vector<ThreadContext> m_threads;
 		std::queue<std::function<void()>> m_tasks;
-		std::condition_variable m_condition;
+		std::condition_variable m_input;
+		std::condition_variable m_output;
 		bool stop = false;
+		
+		std::exception_ptr m_exception;
 	};
 }
