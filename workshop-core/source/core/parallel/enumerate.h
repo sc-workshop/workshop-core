7a21e041c7865cab74d688840c14ee41a8c48f49
Daniil-SV <daniilsivacenko@gmail.com>
2025-09-02 10:20:29 +0300

    wk::parallel now can call worker function with object reference

--- workshop-core/source/core/parallel/enumerate.h
+++ workshop-core/source/core/parallel/enumerate.h
@@ -12,7 +12,7 @@ namespace wk::parallel
 	template<class Iterator>
 	inline void enumerate(
 		Iterator from, Iterator to,
-		std::function<void(TIteratorValue<Iterator>, size_t)> fn,
+		std::function<void(TIteratorValue<Iterator>&, size_t)> fn,
 		std::launch policy = std::launch::deferred | std::launch::async)
 	{
 		size_t task_count = std::distance(from, to);
@@ -21,7 +21,7 @@ namespace wk::parallel
 
 		auto it = from;
 		for (size_t i = 0; i < task_count; i++) {
-			tasks[i] = std::async(policy, fn, *it++, unsigned(i));
+			tasks[i] = std::async(policy, fn, std::ref(*it++), unsigned(i));
 		}
 
 		for (size_t i = 0; i < task_count; ++i) tasks[i].wait();
