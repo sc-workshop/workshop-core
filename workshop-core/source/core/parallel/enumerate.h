9cb79ccffd14b7562ab573814d1cde6bdd78aad1
Daniil-SV <daniilsivacenko@gmail.com>
2025-09-23 19:58:18 +0300

    modernized parallel enumerate

--- workshop-core/source/core/parallel/enumerate.h
+++ workshop-core/source/core/parallel/enumerate.h
@@ -6,13 +6,10 @@
 
 namespace wk::parallel
 {
-	template<class T>
-	using TIteratorValue = typename std::iterator_traits<T>::value_type;
-
-	template<class Iterator>
+	template<std::input_iterator Iterator, typename Func>
 	inline void enumerate(
 		Iterator from, Iterator to,
-		std::function<void(TIteratorValue<Iterator>&, size_t)> fn,
+		Func&& fn,
 		std::launch policy = std::launch::deferred | std::launch::async)
 	{
 		size_t task_count = std::distance(from, to);
@@ -20,8 +17,8 @@ namespace wk::parallel
 		std::vector<std::future<void>> tasks(task_count);
 
 		auto it = from;
-		for (size_t i = 0; i < task_count; i++) {
-			tasks[i] = std::async(policy, fn, std::ref(*it++), unsigned(i));
+		for (size_t idx = 0; idx < task_count; idx++) {
+			tasks[idx] = std::async(policy, fn, std::ref(*it++), idx);
 		}
 
 		for (size_t i = 0; i < task_count; ++i) tasks[i].wait();
