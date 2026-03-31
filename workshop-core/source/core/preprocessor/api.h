66a6167506edd8db72f8d106eb1d131aefd2f012
Daniil-SV <daniilsivacenko@gmail.com>
2026-03-31 14:16:29 +0300

    added inline macro for preprocessor api

--- workshop-core/source/core/preprocessor/api.h
+++ workshop-core/source/core/preprocessor/api.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#if defined(_MSC_VER)
+    #define WK_INLINE __forceinline
+#elif defined(__GNUC__) || defined(__clang__)
+    #define WK_INLINE __attribute__((always_inline))
+#else
+    #define WK_INLINE inline
+#endif
+
 #ifdef _WIN32
 	#if defined(WK_DLL_EXPORT) && (WK_DLL_EXPORT==1)
 		#define WORKSHOP_API __declspec(dllexport)
